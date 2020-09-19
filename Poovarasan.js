08787908358c2c8eabe294f0baec8b0df4f786be
poovarasan <poovarasan1999skp@gmail.com>
2020-09-19 17:31:18 +0530

    daii nanba entha schema okva da

--- Poovarasan.js
+++ Poovarasan.js
@@ -1,6 +1,36 @@
-var fName = "Poo";
-var mName = "varasan";
-var lName = "S";
+// var fName = "Poo";
+// var mName = "varasan";
+// var lName = "S";
 
-var fullName = `${fName}${mName}.${lName}`;
-console.log(fullName);
+// var fullName = `${fName}${mName}.${lName}`;
+// console.log(fullName);
+const mongoose = require("mongoose");
+const ProductSchema = mongoose.Schema({
+  _id: [Schema.Types.ObjectId],
+  product_name: { type: String, required: true },
+  price: { type: Number, required: true },
+  offer: { type: Number, required: true },
+
+  category: { type: String, required: true },
+  sub_category: { type: String, required: true },
+  product_count: {
+    s: { type: Number, required: true },
+    m: { type: Number, required: true },
+    l: { type: Number, required: true },
+    xl: { type: Number, required: true },
+    xxl: { type: Number, required: true },
+  },
+  description: { type: String, required: true },
+  timestamp: { type: TimeRanges, required: true },
+  product_image: { type: String, required: true },
+
+  ratings: {
+    total_rating: { type: Number, required: true },
+    count: { type: Number, required: true },
+  },
+  sold_by: [Schema.Types.ObjectId],
+  solder_name: { type: String, required: true },
+
+  //productDescriptionReviews: { type: String, required: true },
+});
+module.exports = mongoose.model("poovarasan", ProductSchema);
